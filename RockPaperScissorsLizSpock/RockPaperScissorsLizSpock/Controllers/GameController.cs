1f9d5c31c89faaffebd8a38716593f8f09ec2ae7
Kamil EL Kohen <kamil.el.kohen@avanade.com>
2020-05-20 15:08:48 +0200

    Init Game project

---
+++ RockPaperScissorsLizSpock/RockPaperScissorsLizSpock/Controllers/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RockPaperScissorsLizSpock.Controllers
+{
+    [ApiController]
+    [Route("getRps")]
+
+    public class GameController : ControllerBase
+    {
+        // GET: api/Game
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "bienvenu", "salut" };
+        }
+
+        // GET: api/Game/5
+        [HttpGet("{id}", Name = "Get")]
+        public IEnumerable<string> Get(int id)
+        {
+                return new string[] { "valeur " + id , "valeur " + id };
+        }
+
+        // POST: api/Game
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Game/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
