ae9ae7a5390613587571625e31f41e36a9ba51c0
Kamil EL Kohen <kamil.el.kohen@avanade.com>
2020-05-20 15:19:23 +0200

    Add Partie information

---
+++ RockPaperScissorsLizSpock/RockPaperScissorsLizSpock/Services/Partie.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsLizSpock.Services
+{
+    public class Partie
+    {
+        public enum moves { Rock, Paper, Scissors, Lizard, Spock };
+        public enum result { Draw, Win, Loss };
+
+        private moves _userMove;
+        private moves _cpuMove;
+        private result _result;
+
+        public Partie(moves inUserMove, moves inCpuMove)
+        {
+            this._userMove = inUserMove;
+            this._cpuMove = inCpuMove;
+        }
+
+        public result getResult()
+        {
+            generateResult();
+            return this._result;
+        }
+
+        private void generateResult()
+        {
+            this._result = result.Draw;
+            switch (_userMove)
+            {
+                case moves.Rock:
+                    if (_cpuMove == moves.Scissors || _cpuMove == moves.Lizard)
+                        this._result = result.Win;
+                    else if (_cpuMove == moves.Spock || _cpuMove == moves.Paper)
+                        this._result = result.Loss;
+                    break;
+                case moves.Paper:
+                    if (_cpuMove == moves.Rock || _cpuMove == moves.Spock)
+                        this._result = result.Win;
+                    else if (_cpuMove == moves.Lizard || _cpuMove == moves.Scissors)
+                        this._result = result.Loss;
+                    break;
+                case moves.Scissors:
+                    if (_cpuMove == moves.Paper || _cpuMove == moves.Lizard)
+                        this._result = result.Win;
+                    else if (_cpuMove == moves.Spock || _cpuMove == moves.Rock)
+                        this._result = result.Loss;
+                    break;
+                case moves.Lizard:
+                    if (_cpuMove == moves.Paper || _cpuMove == moves.Spock)
+                        this._result = result.Win;
+                    else if (_cpuMove == moves.Scissors || _cpuMove == moves.Rock)
+                        this._result = result.Loss;
+                    break; ;
+                case moves.Spock:
+                    if (_cpuMove == moves.Scissors || _cpuMove == moves.Rock)
+                        this._result = result.Win;
+                    else if (_cpuMove == moves.Paper || _cpuMove == moves.Lizard)
+                        this._result = result.Loss;
+                    break;
+                default:
+                    this._result = result.Draw;
+                    break;
+            }
+        }
+
+        public moves UsersMove
+        {
+            get
+            {
+                return this._userMove;
+            }
+            set
+            {
+                this._userMove = value;
+            }
+        }
+
+        public moves CpuMove
+        {
+            get
+            {
+                return this._cpuMove;
+            }
+            set
+            {
+                this._cpuMove = value;
+            }
+        }
+    }
+}
